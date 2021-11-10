16426824ccd3d1813339440f3a03def87d2c2d63
Yu-Kabuto <inoue.kazuya.9f@moegi.waseda.jp>
2021-11-10 18:33:11 +0900

    new page

---
+++ hiramekiroomcontrol.js
@@ -0,0 +1,13 @@
+//document.addEventListener('DOMContentLoaded',function(){
+    let correct = ['路','gasoline'];
+    var num;
+    //var buttons = document.querySelectorAll("input[id='buttons']");
+    function judgement(num){
+	var answer = document.getElementById("a"+num).value;
+	    if(answer == correct[num]){
+		document.getElementById("b"+num).value = "正解です!";
+	    } else {
+		document.getElementById("b"+num).value = "不正解です";
+	    }
+    }
+//},false);
