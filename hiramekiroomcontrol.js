c5e90753bba0bac4fba116be3d67e93fc9fd57d3
Yu-Kabuto <inoue.kazuya.9f@moegi.waseda.jp>
2022-01-29 14:48:25 +0900

    add a question and some check

--- hiramekiroomcontrol.js
+++ hiramekiroomcontrol.js
@@ -1,5 +1,5 @@
 //document.addEventListener('DOMContentLoaded',function(){
-let correct = ['路','gasoline','石鹸'];
+let correct = ['路','gasoline','石鹸','わたしだめしまいにいましめだしたわ'];
     var num;
     //var buttons = document.querySelectorAll("input[id='buttons']");
     function judgement(num){
